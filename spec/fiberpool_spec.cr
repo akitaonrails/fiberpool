5127a932b0add1035c915719d52682ff6084be32
Fabio Akita <fabioakita@gmail.com>
2016-06-08 18:51:23 -0300

    adding a spec to assert that the worker fibers are indeed running concurrently

--- spec/fiberpool_spec.cr
+++ spec/fiberpool_spec.cr
@@ -1,4 +1,5 @@
 require "./spec_helper"
+require "benchmark"
 
 describe Fiberpool do
   it "should deplete the queue and add each item to a results accumulator - just tests the external API" do
@@ -9,5 +10,20 @@ describe Fiberpool do
       results << item
     end
     results.size.should eq(100)
+  end
+
+  it "should do the work 5x faster with the queue" do
+    m = Benchmark.measure("") {
+      results = [] of Int32
+      queue = (1..10).to_a
+      pool = Fiberpool.new(queue, 5)
+      pool.run do |item|
+        sleep 1
+        results << item
+      end
+    }
+    # if they had run in sequence, it should take 10 seconds of sleep
+    # but with a queue of 5 fibers, it should take around 2 seconds
+    (m.real < 3).should eq(true)
   end
 end
